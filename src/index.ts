fb342ae5efa6c24c5c1275292b188ac1ebd09b3e
Rodrigo Campos Cabral <142432355+Rodcamppos@users.noreply.github.com>
2025-06-07 00:22:13 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,50 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; 
-import http from 'http'; 
-import userRoutes from './routes/userRoutes';
-import { ChatController } from './controllers/chatController';
-import chatRoutes from './routes/chatRoutes';
+import cors from 'cors';
+import http from 'http';
 import prisma from './prisma';
+import userRoutes from './routes/userRoutes';
+import chatRoutes from './routes/chatRoutes';
+import authRouter from './authRoutes';
+import { ChatController } from './controllers/chatController';
 
 dotenv.config();
 
 const app = express();
-
-const server = http.createServer(app);
-
-const chatController = new ChatController(server);
 
 prisma.$connect()
   .then(() => console.log('Conectado ao PostgreSQL via Prisma!'))
   .catch(err => console.error('Erro na conexão Prisma → PostgreSQL:', err));
 
 app.use(cors({
-  origin: '*',  
-  methods: ['GET', 'POST'],    
-  allowedHeaders: ['Content-Type', 'Authorization'],  
-  credentials: true,  
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
-
 app.use(express.json());
+
 app.use('/api', userRoutes);
 app.use('/api', chatRoutes);
+app.use('/api/auth', authRouter);
 app.use(express.static('src/websocket'));
 
-const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const chatController = new ChatController(server);
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 process.on('SIGINT', async () => {
-  console.log("O servidor está sendo encerrado. As conversas estão sendo terminadas.")
-
+  console.log("O servidor está sendo encerrado. As conversas estão sendo terminadas.");
   await prisma.historic.updateMany({
-    where: { terminated: false},
-    data : {
+    where: { terminated: false },
+    data: {
       terminated: true,
       endedAt: new Date()
     }
-  })
-  
-  process.exit(0)
-})
+  });
+  process.exit(0);
+});
